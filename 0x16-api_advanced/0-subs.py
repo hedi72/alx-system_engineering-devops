7bee425bdcbe6bc41437e45c569b6d0a60cbcc20
hedi72 <heditaieb72@gmail.com>
2024-01-17 05:19:03 -0800

    task1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,29 +1,20 @@
 #!/usr/bin/python3
+""" a function that queries the Reddit API and returns
+the number of subscribers (not active users, total subscribers)
+for a given subreddit.
 """
-0-subs
-"""
+
 import requests
+
 
 def number_of_subscribers(subreddit):
     # Construct the URL for the subreddit's about page in JSON format
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    
-    # Set a custom User-Agent to avoid Too Many Requests error
-    headers = {'User-Agent': 'MyRedditBot/1.0'}
-    
-    # Make the request to the Reddit API
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    
+    response = requests.get(url, headers={'User-Agent': 'app/1.0'})
     if response.status_code == 200:
         # Parse the JSON response
         data = response.json()
-        
-        # Check if the 'subscribers' key is present in the response
-        if 'subscribers' in data['data']:
-            return data['data']['subscribers']
-        else:
-            # 'subscribers' key not found (invalid subreddit)
-            return 0
+        subscribers = data['data']['subscribers']
+        return subscribers
     else:
-        # Invalid subreddit or other error
         return 0
